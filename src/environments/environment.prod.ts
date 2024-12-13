6491854e501583195ca98bdaa8ef7ba90ebbcac5
leandrocsr <leandrocsr@souunisuam.com.br>
2024-12-13 10:21:35 -0300

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAx3gNHuysTSY-EY7SBaP8b8gfeT03AuN4",
+    authDomain: "nerdsphera.firebaseapp.com",
+    projectId: "nerdsphera",
+    storageBucket: "nerdsphera.firebasestorage.app",
+    messagingSenderId: "322342536799",
+    appId: "1:322342536799:web:27d325bc75f1d008383dea",
+    measurementId: "G-CJR855419G"
+  }
 };
